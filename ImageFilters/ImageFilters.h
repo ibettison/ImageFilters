b508d4e6c39f20fe4ca9481e10115b6e6a6a522d
Ian Bettison <ian.bettison@ncl.ac.uk>
2014-05-15 09:36:03 +0100

    Initial Commit

---
+++ ImageFilters/ImageFilters.h
@@ -0,0 +1,13 @@
+//
+//  ImageFilters.h
+//  ImageFilters
+//
+//  Created by Ian Bettison on 15/05/2014.
+//  Copyright (c) 2014 Ian Bettison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageFilters : NSObject
+
+@end
---
+++ ImageFilters/ImageFilters.m
@@ -0,0 +1,13 @@
+//
+//  ImageFilters.m
+//  ImageFilters
+//
+//  Created by Ian Bettison on 15/05/2014.
+//  Copyright (c) 2014 Ian Bettison. All rights reserved.
+//
+
+#import "ImageFilters.h"
+
+@implementation ImageFilters
+
+@end
