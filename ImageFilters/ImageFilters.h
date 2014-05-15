321abc1040523cea1bb9e8ad9c291cf69d36bae4
Ian Bettison <ian.bettison@ncl.ac.uk>
2014-05-15 11:37:59 +0100

    Finished version of a static library

--- ImageFilters/ImageFilters.h
+++ ImageFilters/ImageFilters.h
@@ -6,8 +6,16 @@
 //  Copyright (c) 2014 Ian Bettison. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
 @interface ImageFilters : NSObject
+
+@property (nonatomic, readonly) UIImage *originalImage;
+
+- (id)initWithImage:(UIImage *)image;
+- (UIImage *)grayScaleImage;
+- (UIImage *)oldImageWithIntensity:(CGFloat)level;
+
 
 @end
